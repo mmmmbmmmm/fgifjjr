8eb00a3c8c97481389cb5285b42f01afd74d87d0
user <user@C1010>
2021-11-30 08:52:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <math.h>
+
+class Triangle {
+private:
+	double a = 0, b = 0, c = 0;
+public:
+	Triangle(double v1, double v2, double v3) {
+		a = v1;
+		b = v2;
+		c = v3;
+	};
+
+	~Triangle() {
+		a = 0;
+		b = 0;
+		c = 0;
+	};
+
+	void perimetr() {
+		double p = 0;
+		p = a + b + c;
+		std::cout << "P = "<< p<< std::endl;
+	}
+
+	void square() {
+		double p = (a + b + c) / 2;
+		double S = sqrt(p * (p - a) * (p - b) * (p - c));
+		std::cout << "S = " << S;
+	}
+};
+
+int main() {
+	double a, b, c;
+	std::cin >> a >> b >> c;
+	Triangle obj(a, b, c);
+	obj.perimetr();
+	obj.square();
+	return 0;
+}
+
